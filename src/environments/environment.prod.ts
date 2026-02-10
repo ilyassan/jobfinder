12c35da06514ba5e1f2e99027cc0869f3f13e391
ilyass anida <anidailyass@gmail.com>
2026-02-10 22:44:56 +0100

    feat: configure http client and environment settings

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://www.arbeitnow.com/api',
+  jsonServerUrl: 'http://localhost:5000'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://www.arbeitnow.com/api',
+  jsonServerUrl: 'http://localhost:5000'
+};
